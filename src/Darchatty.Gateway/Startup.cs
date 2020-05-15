6517eec04647309fe1d1874e0d5bbf626848835d
Liam Morrow <liam@citrusad.com>
2020-05-15 15:48:03 +1000

    Fixed failure to startup, null poiunter wasa red herring

--- src/Darchatty.Gateway/Startup.cs
+++ src/Darchatty.Gateway/Startup.cs
@@ -1,36 +1,90 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Darchatty.Gateway.Hubs;
+using Darchatty.Orleans.GrainInterfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OrgnalR.SignalR;
+using Orleans;
+using Orleans.Clustering.Kubernetes;
+using Orleans.Configuration;
 
 namespace Darchatty.Gateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration config)
         {
-            services.AddCors();
-            services.AddSignalR();
+            configuration = config;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(serviceProvider =>
+            {
+                var clientBuilder = new ClientBuilder()
+                    // Clustering information
+                    .Configure<ClusterOptions>(options =>
+                    {
+                        options.ClusterId = "darchatty-cluster";
+                        options.ServiceId = "darchatty";
+                    })
+                    .UseOrgnalR()
+                    .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IChatGrain).Assembly))
+                    .ConfigureLogging(x => x.AddConsole());
+                if (configuration.GetValue("OrleansClusteringMode", "local") == "local")
+                {
+                    clientBuilder.UseLocalhostClustering(serviceId: "darchatty", clusterId: "darchatty-cluster");
+                }
+                else
+                {
+                    clientBuilder.UseKubeGatewayListProvider();
+                }
+
+                return clientBuilder.Build();
+            });
+            services.AddSignalR()
+                .UseOrgnalR();
+
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(s => true)
+                    .AllowCredentials();
+            }));
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+#pragma warning disable VSTHRD002
+            var logger = app.ApplicationServices.GetRequiredService<ILoggerFactory>();
+            var lifetime = app.ApplicationServices.GetRequiredService<IHostApplicationLifetime>();
+            app.ApplicationServices.GetRequiredService<IClusterClient>().Connect(async x =>
+            {
+                logger.CreateLogger("Silo connect").LogInformation("Could not connect to orleans silo!  Retrying in 5 seconds.");
+                if (lifetime.ApplicationStopping.IsCancellationRequested)
+                {
+                    return false;
+                }
+
+                await Task.Delay(5000);
+                return true;
+            }).GetAwaiter().GetResult();
 
             app.UseRouting();
-            app.UseCors(c => c.AllowAnyMethod().AllowAnyOrigin().AllowAnyHeader());
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
